b2a1ddbf9cd7578b14237b233b48e6f0ed6ee9e6
laizc <1049024592@qq.com>
2026-05-07 17:44:29 +0800

    添加 post 方法

--- controllers/default.go
+++ controllers/default.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -15,11 +18,20 @@ func (c *MainController) Get() {
 	c.TplName = "index.tpl"
 }
 
+type UserReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func (c *MainController) Post() {
+	var req UserReq
+	json.Unmarshal(c.Ctx.Input.RequestBody, &req)
+	fmt.Println(req.Username, req.Password)
+
 	c.Data["json"] = map[string]interface{}{
 		"success": true,
 		"code":    200,
-		"msg":     "这是返回的json数据",
+		"msg":     "你传参 name 是" + req.Username,
 	}
 	c.ServeJSON()
 }
