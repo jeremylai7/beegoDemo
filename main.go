cb2a52b9594f4042ab699d7ffd4d754c98285fd9
laizc <1049024592@qq.com>
2026-05-11 09:49:48 +0800

    数据库配置转移到配置文件

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"awesomeProject1/models"
 	_ "awesomeProject1/routers"
+	"fmt"
 
 	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
@@ -21,12 +22,21 @@ func init() {
 	// 打印 sql
 	orm.Debug = true
 
+	driver, _ := beego.AppConfig.String("db_driver")
+	user, _ := beego.AppConfig.String("db_user")
+	password, _ := beego.AppConfig.String("db_password")
+	host, _ := beego.AppConfig.String("db_host")
+	port, _ := beego.AppConfig.String("db_port")
+	dbName, _ := beego.AppConfig.String("db_name")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		user,
+		password,
+		host, port,
+		dbName)
+
 	// 注册数据库
-	orm.RegisterDataBase(
-		"default",
-		"mysql",
-		"root:lzc123456@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=true&loc=Local",
-	)
+	orm.RegisterDataBase("default", driver, dsn)
 
 	// 注册模型
 	orm.RegisterModel(new(models.User))
